0e83f85b3505370818fa1e8ed682e788203cb39b
Aaron McDonald <sergeantbeast456@gmail.com>
2022-09-26 12:06:16 -0400

    Functioning program using C++ built in reverse method

--- CPP_Reverse_String/main.cpp
+++ CPP_Reverse_String/main.cpp
@@ -3,21 +3,29 @@
 // This program accepts a string in from the user and outputs that string in reverse
 
 #include <iostream>
-
+#include <string>
 
 using namespace std;
 
+// Creating the function from the ground up
 string reverse_string(const string& string_in) {
 	// FIXME - Reverse the string reference and return
+	return "";
+}
+
+// Using the built in reverse function
+string reverse_string_cpp(const string& string_in) {
+	string string_copy = string_in;
+	reverse(string_copy.begin(), string_copy.end());
+	return string_copy;
 }
 
 int main() {
 	string user_in;
 
 	cout << "Please enter a string that you want reversed: ";
-	cin >> user_in;
-
-	cout << "String Reversed: " << reverse_string(user_in) << endl;
+	getline(cin, user_in);
+	cout << "String Reversed: " << reverse_string_cpp(user_in) << endl;
 
 	return 0;
 }
