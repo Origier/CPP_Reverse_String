3ba938993fedd02a2f6a1c363efe99c84b808edb
Aaron McDonald <sergeantbeast456@gmail.com>
2022-09-26 12:26:49 -0400

    Built a reverse function from the ground up using iterators

--- CPP_Reverse_String/main.cpp
+++ CPP_Reverse_String/main.cpp
@@ -9,8 +9,26 @@ using namespace std;
 
 // Creating the function from the ground up
 string reverse_string(const string& string_in) {
-	// FIXME - Reverse the string reference and return
-	return "";
+	// Copying the string and creating iterators for the beginning and end of the string to iterate through it from both sides
+	string string_copy = string_in;
+	std::string::iterator string_start = string_copy.begin();
+	std::string::iterator string_end = string_copy.end();
+	char temp = '\0';
+	int loop_stop = string_copy.size() / 2; // Integer division to ensure the loop stops modifying before reaching the middle
+
+	// Looping through the string up to the mid point and swapping the end point values
+	for (int i = 0; i < loop_stop; i++) {
+		// Swapping the values for the pointers on both ends of the string
+		string_end--; // Decrementing the endpoint first since this is the null character on the string
+		temp = *string_start;
+		*string_start = *string_end;
+		*string_end = temp;
+
+		// Incrementing and decrementing the pointers
+		string_start++;
+	}
+
+	return string_copy;
 }
 
 // Using the built in reverse function
@@ -25,7 +43,7 @@ int main() {
 
 	cout << "Please enter a string that you want reversed: ";
 	getline(cin, user_in);
-	cout << "String Reversed: " << reverse_string_cpp(user_in) << endl;
+	cout << "String Reversed: " << reverse_string(user_in) << endl;
 
 	return 0;
 }
